8aec24e2314ccd1b2841311bb116c8c979453546
Nandhini13panduranga <pjnandhini25@gmail.com>
2019-04-01 11:07:30 +0530

    New Project added

---
+++ src/org/tcs/com/program.java
@@ -0,0 +1,18 @@
+package org.tcs.com;
+
+public class program {
+	public static void main(String[] args) {
+		int num[] = new int[5];
+
+		num[0] = 10;
+		num[1] = 20;
+		num[2] = 30;
+		num[3] = 40;
+		num[4] = 50;
+		num[2] = 300;
+		int len = num.length;
+		System.out.println(len);
+
+	}
+
+}
